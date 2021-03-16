05bfcd70a9c08ce22faa558ec61b902e0098bac1
Numan Ahmed <mdnmnahmed@gmail.com>
2021-03-16 22:53:14 +0530

    Expenses all Routes created

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { allExpenses } = require('../controllers/expensesController')
+const { getExpenses, addExpense, getSpecificExpense, deleteExpense, updateExpense } = require('../controllers/expensesController');
 const router = express.Router();
 
-router.get('/all-expenses', allExpenses);
+router.get('/get-expenses', getExpenses);
+router.post('/add-expense', addExpense);
+router.get('/get-specific-expense/:expense_id', getSpecificExpense);
+router.delete('/delete-expense/:expense_id', deleteExpense);
+router.put('/update-expense/:expense_id', updateExpense);
 
 
 module.exports = router;

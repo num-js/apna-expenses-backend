0d8920f9c132f87614b1d52d28fdfa63c7045d40
Numan Ahmed <mdnmnahmed@gmail.com>
2021-03-16 22:54:29 +0530

    Connection with MongoDB & few packages imported

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 
+//Middlewares
+app.use(bodyParser.json());
+// app.use(cors());
 
-app.use('/expenses', require('./routes/routes'));
+//Routes
+app.use('/users', require('./routes/routes'));
+
 
 app.get('/', (req, res) => {
-    res.send('Hello Numan');
+    res.send('<div align="center"><h1>Apna Expenses</h1></div>');
 });
+
+
+//Connection with DB
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+    .then((res) => {
+        console.log("Connected with MongoDB ");
+    })
+    .catch((err) => {
+        console.log("Error in with MongoDB ", err);
+    });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`App sterted at: http://localhost:${PORT}/`);
+    console.log(`App sterted at: http://localhost:5000/`);
 });
